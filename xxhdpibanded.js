af8df94351ed1dfc81d298a31ba4ea0077086450
Alex Miller <arandompackage@gmail.com>
2016-06-25 12:49:11 -0600

    Added JSON file
    JSON File

---
+++ xxhdpibanded.js
@@ -0,0 +1,71 @@
+{
+    "wallpapers": {
+        "category": [
+            {
+                "name": "xhdpi",
+                "wallpaper": [
+                    {
+                        "author": "A Random Package",
+                        "url": "https://dl.dropboxusercontent.com/s/xs2p3oe3xc2tsjs/Napili_Rain.jpg?dl=0",
+                        "thumbUrl": "https://dl.dropboxusercontent.com/s/eamvtldrosgp2bw/napili_small.png?dl=0",
+                        "name": "Napili Rain"
+                    },
+                    {
+                        "author": "A Random Package",
+                        "url": "https://dl.dropboxusercontent.com/s/wuuy4x4u0xw20bp/bandedone.jpg?dl=0",
+                        "thumbUrl": "https://dl.dropboxusercontent.com/s/cgblqqp8r4f6rfg/one_small.jpg?dl=0",
+                        "name": "Settled"
+                    },
+                    {
+                        "author": "A Random Package",
+                        "url": "https://dl.dropboxusercontent.com/s/s6vu6c8twjq2qfi/bandedtwo.png?dl=0",
+                        "thumbUrl": "https://dl.dropboxusercontent.com/s/pfyf1f1seb0jg6g/two_small.jpg?dl=0",
+                        "name": "Two"
+                    },
+                    {
+                        "author": "A Random Package",
+                        "url": "https://dl.dropboxusercontent.com/s/q49mlugxxooqs2l/bandedthree.jpg?dl=0",
+                        "thumbUrl": "https://dl.dropboxusercontent.com/s/36hletnbezdza34/three_small.jpg?dl=0",
+                        "name": "Three"
+                    },
+                    {
+                        "author": "YAbeats",
+                        "url": "https://dl.dropboxusercontent.com/s/k1chvnxnlw4epde/bandedfour.png?dl=0",
+                        "thumbUrl": "https://dl.dropboxusercontent.com/s/e213ceffxj17mme/four_small.png?dl=0",
+                        "name": "Four"
+                    },
+                    {
+                        "author": "YAbeats // Photo Cred: Richard Colon",
+                        "url": "https://dl.dropboxusercontent.com/s/d0b08qgagbtx48p/Binho.png?dl=0",
+                        "thumbUrl": "https://dl.dropboxusercontent.com/s/vd6ep8si5ji0o08/binho_small.jpg?dl=0",
+                        "name": "Binho"
+                    },
+                    {
+                        "author": "YAbeats // Photo Cred: Richard Colon",
+                        "url": "https://dl.dropboxusercontent.com/s/5qbrjimv2ycru0h/CastAway.png?dl=0",
+                        "thumbUrl": "https://dl.dropboxusercontent.com/s/7ozsbeqtkxvuwty/castaway_small.jpg?dl=0",
+                        "name": "Castaway"
+                    },
+                    {
+                        "author": "YAbeats // Photo Cred: Richard Colon",
+                        "url": "https://dl.dropboxusercontent.com/s/3i6nfkti64zk078/CastAway%28Noloc%29.jpg?dl=0",
+                        "thumbUrl": "https://dl.dropboxusercontent.com/s/7ozsbeqtkxvuwty/castaway_small.jpg?dl=0",
+                        "name": "Castaway(Noloc)"
+                    },
+                    {
+                        "author": "Arandompackage",
+                        "url": "https://dl.dropboxusercontent.com/s/l23hcknyt33pyml/banded_1.jpg?dl=0",
+                        "thumbUrl": "https://dl.dropboxusercontent.com/s/0ydjvo76089nq72/five_small.jpg?dl=0",
+                        "name": "Five"
+                    },
+                    {
+                        "author": "YAbeats // Photo Cred: Richard Colon",
+                        "url": "https://dl.dropboxusercontent.com/s/f7pmr256i2tb3gb/Binho_banded.jpg?dl=0",
+                        "thumbUrl": "https://dl.dropboxusercontent.com/s/bfiw4herimsikto/binho_small.png?dl=0",
+                        "name": "Binho(Banded)"
+                    }
+                ]
+            }
+        ]
+    }
+}
